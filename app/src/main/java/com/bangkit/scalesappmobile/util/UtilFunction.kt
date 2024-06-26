fa61e923c3ee32c78b7b04cbbfc40fbc0abb8de3
kusandipriyono <andipriyono94@gmail.com>
2024-06-26 22:34:47 +0700

    update pdf view

--- app/src/main/java/com/bangkit/scalesappmobile/util/UtilFunction.kt
+++ app/src/main/java/com/bangkit/scalesappmobile/util/UtilFunction.kt
@@ -17,12 +17,19 @@ import com.itextpdf.io.image.ImageDataFactory
 import com.itextpdf.kernel.colors.ColorConstants
 import com.itextpdf.kernel.font.PdfFontFactory
 import com.itextpdf.kernel.geom.PageSize
+import com.itextpdf.kernel.geom.Rectangle
 import com.itextpdf.kernel.pdf.PdfDocument
 import com.itextpdf.kernel.pdf.PdfWriter
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas
 import com.itextpdf.layout.Document
+import com.itextpdf.layout.borders.SolidBorder
+import com.itextpdf.layout.element.Cell
+import com.itextpdf.layout.element.Image
 import com.itextpdf.layout.element.Paragraph
+import com.itextpdf.layout.element.Table
 import com.itextpdf.layout.properties.HorizontalAlignment
 import com.itextpdf.layout.properties.TextAlignment
+import com.itextpdf.layout.properties.VerticalAlignment
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
@@ -165,63 +172,159 @@ fun formatDate(date: String): String {
 }
 
 fun createPdfFile(context: Context, data: AllForm): File? {
-    val directoryPath = context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)?.absolutePath
-        ?: return null
-
+    val directoryPath = context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)?.absolutePath ?: return null
     val file = File("$directoryPath/scales.pdf")
 
     return try {
         val pdfWriter = PdfWriter(file)
         val pdfDocument = PdfDocument(pdfWriter)
         val document = Document(pdfDocument, PageSize.A4)
+        val margin = 36f
+        document.setMargins(margin, margin, margin, margin)
 
-        // Set the document margins
-        document.setMargins(36f, 36f, 36f, 36f)
+        val font = PdfFontFactory.createFont(StandardFonts.HELVETICA_OBLIQUE)
 
-        // Load the image from drawable
-        val headerImageId = R.drawable.logo_myr // Replace with your drawable image resource ID
+        // Header
+        val headerImageId = R.drawable.logo_myr
         val bitmap = BitmapFactory.decodeResource(context.resources, headerImageId)
             ?: throw FileNotFoundException("Drawable resource not found: $headerImageId")
-        val stream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
-        val headerImage =
-            com.itextpdf.layout.element.Image(ImageDataFactory.create(stream.toByteArray()))
+        val headerImage = Image(ImageDataFactory.create(bitmapToByteArray(bitmap)))
         headerImage.setWidth(50f)
-        headerImage.setHorizontalAlignment(HorizontalAlignment.LEFT)
-        // Add header image to the PDF
-        document.add(headerImage)
+        headerImage.setHorizontalAlignment(HorizontalAlignment.CENTER)
 
-        // Add title
-        val title = Paragraph("DEPARTEMENT TEKNIK\nCALIBRATION")
-            .setFontSize(18f)
-            .setTextAlignment(TextAlignment.CENTER)
-            .setBold()
-        document.add(title)
+        val headerTable = Table(floatArrayOf(100f, 285f, 190f))
+        headerTable.addCell(Cell().add(headerImage).setBorder(SolidBorder(ColorConstants.GRAY, 1f)))
+        headerTable.addCell(
+            Cell().add(
+                Paragraph("PT. MAYORA INDAH, Tbk\nJatake-Tangerang")
+                    .setFontSize(15f)
+                    .setTextAlignment(TextAlignment.CENTER)
+                    .setFontColor(ColorConstants.RED)
+            ).setBorder(SolidBorder(ColorConstants.GRAY, 1f))
+        )
+        headerTable.addCell(
+            Cell().add(
+                Paragraph("SERTIFIKAT KALIBRASI")
+                    .setFontSize(10f)
+                    .setTextAlignment(TextAlignment.CENTER)
+                    .setFontColor(ColorConstants.BLUE)
+                    .setBold()
+            ).setBorder(SolidBorder(ColorConstants.GRAY, 1f))
+        )
+        document.add(headerTable)
 
-        // Add the form data
-        document.add(Paragraph("\nIDENTITAS ALAT").setBold())
-        document.add(Paragraph("Deskripsi Alat: ${data.scale.name}"))
+        // Department
+        document.add(
+            Paragraph("DEPARTEMENT TEKNIK\nCALIBRATION")
+                .setFontSize(15f)
+                .setTextAlignment(TextAlignment.CENTER)
+                .setFontColor(ColorConstants.BLUE)
+                .setBold()
+        )
+
+        // Identitas Alat
+        document.add(
+            Paragraph("IDENTITAS ALAT")
+                .setFontSize(10f)
+                .setTextAlignment(TextAlignment.LEFT)
+                .setFontColor(ColorConstants.BLACK)
+                .setBold()
+        )
+
+        val dataIdentitasAlat = Table(floatArrayOf(200f, 350f))
+        with(dataIdentitasAlat) {
+            addCell(Cell().add(Paragraph("Nomor Alat")))
+            addCell(Cell().add(Paragraph(data.scale.measuringEquipmentIdNumber)))
+            addCell(Cell().add(Paragraph("Nama Alat")))
+            addCell(Cell().add(Paragraph(data.scale.name)))
+            addCell(Cell().add(Paragraph("Merk Pabrik / Tipe")))
+            addCell(Cell().add(Paragraph("${data.scale.brand} / ${data.scale.kindType}")))
+            addCell(Cell().add(Paragraph("Nomor Seri")))
+            addCell(Cell().add(Paragraph(data.scale.serialNumber)))
+            addCell(Cell().add(Paragraph("Kapasitas")))
+            addCell(Cell().add(Paragraph("${data.scale.rangeCapacity}${data.scale.unit}")))
+        }
+        document.add(dataIdentitasAlat)
+
+        // Identitas Pemilik
+        document.add(
+            Paragraph("IDENTITAS PEMILIK")
+                .setFontSize(10f)
+                .setTextAlignment(TextAlignment.LEFT)
+                .setFontColor(ColorConstants.BLACK)
+                .setBold()
+        )
+
+        val dataIdentitasPemilik = Table(floatArrayOf(200f, 350f))
+        with(dataIdentitasPemilik) {
+            addCell(Cell().add(Paragraph("Nama Pemilik")))
+            addCell(Cell().add(Paragraph(data.scale.parentMachineOfEquipment)))
+            addCell(Cell().add(Paragraph("Metoda Kalibrasi")))
+            addCell(Cell().add(Paragraph(data.calibrationMethod)))
+            addCell(Cell().add(Paragraph("Acuan Standard")))
+            addCell(Cell().add(Paragraph(data.reference)))
+            addCell(Cell().add(Paragraph("Standar Kalibrasi")))
+            addCell(Cell().add(Paragraph(data.standardCalibration)))
+            addCell(Cell().add(Paragraph("Tanggal Kalibrasi")))
+            addCell(Cell().add(Paragraph(formatDate(data.createdAt.toString()))))
+            addCell(Cell().add(Paragraph("Tempat Kalibrasi")))
+            addCell(Cell().add(Paragraph(data.scale.location)))
+            addCell(Cell().add(Paragraph("Suhu")))
+            addCell(Cell().add(Paragraph("${data.suhu}°C")))
+            addCell(Cell().add(Paragraph("Keterangan Kalibrasi")))
+            addCell(Cell().add(Paragraph(data.resultCalibration)))
+            addCell(Cell().add(Paragraph("Berlaku Sampai")))
+            addCell(Cell().add(Paragraph(formatDate(data.validUntil.toString()))))
+        }
+        document.add(dataIdentitasPemilik)
+
+        // Hasil Kalibrasi
+        document.add(
+            Paragraph("HASIL KALIBRASI")
+                .setFontSize(10f)
+                .setTextAlignment(TextAlignment.LEFT)
+                .setFontColor(ColorConstants.BLACK)
+                .setBold()
+        )
+
+        val dataHasilKalibrasi = Table(floatArrayOf(200f, 350f))
+        with(dataHasilKalibrasi) {
+            addCell(Cell().add(Paragraph("Posisi anak timbangan di tengah")))
+            addCell(Cell().add(Paragraph(data.readingCenter.toString())))
+            addCell(Cell().add(Paragraph("Posisi anak timbangan di depan")))
+            addCell(Cell().add(Paragraph(data.readingFront.toString())))
+            addCell(Cell().add(Paragraph("Posisi anak timbangan di belakang")))
+            addCell(Cell().add(Paragraph(data.readingBack.toString())))
+            addCell(Cell().add(Paragraph("Posisi anak timbangan di kiri")))
+            addCell(Cell().add(Paragraph(data.readingLeft.toString())))
+            addCell(Cell().add(Paragraph("Posisi anak timbangan di kanan")))
+            addCell(Cell().add(Paragraph(data.readingRight.toString())))
+            addCell(Cell().add(Paragraph("Total")))
+            addCell(Cell().add(Paragraph(data.maxTotalReading.toString()).setFont(font)))
+        }
+        document.add(dataHasilKalibrasi)
 
         // Footer
-        val footer = Paragraph("Tangerang, ${Date()}\n\n\n\n\n")
-            .setFontSize(12f)
-            .setTextAlignment(TextAlignment.RIGHT)
-        document.add(footer)
-
-        val font = PdfFontFactory.createFont(StandardFonts.HELVETICA)
-        val footerSign = Paragraph("CALIBRATOR\n\n(Dept. Head Teknik)")
-            .setFont(font)
-            .setFontSize(10f)
-            .setTextAlignment(TextAlignment.RIGHT)
-            .setFontColor(ColorConstants.GRAY)
-        document.add(footerSign)
+        document.add(
+            Paragraph("Tangerang, ${formatDate(Date().toString())}")
+                .setFontSize(12f)
+                .setTextAlignment(TextAlignment.LEFT)
+        )
 
         // Close document
         document.close()
         pdfDocument.close()
+
         file
     } catch (e: IOException) {
         e.printStackTrace()
         null
     }
 }
+
+private fun bitmapToByteArray(bitmap: Bitmap): ByteArray {
+    val stream = ByteArrayOutputStream()
+    bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+    return stream.toByteArray()
+}
+
